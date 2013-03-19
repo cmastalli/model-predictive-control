1260d162d7b95cd91bbc300937db56a739883237
Carlos Mastalli <carlos.mastalli@iit.it>
2013-03-19 22:11:35 +0000

    Testing an online modifications of this file

--- mpc/include/mpc/Headers_Rene/Model.h
+++ mpc/include/mpc/Headers_Rene/Model.h
@@ -16,16 +16,14 @@ namespace mpc
 
         ~Optimizer();
 	
-
-	/*
-
-	@brief function to define the cost function associated to the MPC problem 
-	@param MPC::Model *myModel		pointer to the process model class "Model"
-	@param int &nWSR			number of working set recalculations
-	@param double *cputime			pointer to the defined time to solve the optimization problem. If NULL, it provides on output 							the actual calculation time of the optimization problem.
-	
-	*/
         virtual void computeOpt(MPC::Model *myModel, int &nWSR, double *cputime);
+
+        /*
+        @brief function to define the cost function associated to the MPC problem
+        @param MPC::Model *myModel		pointer to the process model class "Model"
+        @param int &nWSR			number of working set recalculations
+        @param double *cputime			pointer to the defined time to solve the optimization problem. If NULL, it provides on output the actual calculation time of the optimization problem.
+        */
 
 
 
@@ -33,8 +31,7 @@ namespace mpc
 
 
         private:
-
-
+     	
     }; // Model Predictive Control class
 
 }; // mpc namespace
