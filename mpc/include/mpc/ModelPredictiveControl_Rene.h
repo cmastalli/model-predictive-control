6d5f0a01f098dcb70ed604c75979d094d9e8c4e8
Carlos Mastalli <carlos.mastalli@iit.it>
2013-03-19 22:29:54 +0000

    Improving the comments of code

--- mpc/include/mpc/ModelPredictiveControl_Rene.h
+++ mpc/include/mpc/ModelPredictiveControl_Rene.h
@@ -3,48 +3,45 @@
 
 namespace mpc
 {
-
+    /**
+     @class ModelPredictiveControl
+     @brief Abstract (I'm not sure!!) class to define the model predictive control problem
+     */
     class ModelPredictiveControl
     {
-	public:
-        
-        // Constructor
+	      public:
+            /**
+             @brief Constructor function
+             */
+            ModelPredictiveControl();
 
-        ModelPredictiveControl();
 
-        // Destructor
+            /**
+             @brief Constructor function
+             */
+            ~ModelPredictiveControl();
 
-        ~ModelPredictiveControl();
+            /*
+             @brief Function to specify the settings of all variables within the MPC problem (optimization library, horizon, etc.)
+             @param MPC::Model *myModel		pointer to the model of the plant to be used in the algorithm
+             @param MPC::Optimizer *qpOASES		pointer to the optimization library to be used in the algorithm
+             @param MPC::Simulator *mySimulator	pointer to the simulator class used to predict the states
+             */
+            void resetMPC( MPC::Model *myModel, MPC::Optimizer *qpOASES, MPC::Simulator *mySimulator);
 
-	/*
+            /*
+             @brief function to initialize the calculation of the MPC algorithm
+             @param
+             @param
+             */
+            void initMPCCalc();
 
-	@brief function to specify the settings of all variables within the MPC problem (optimization library, horizon, etc.)
-	@param MPC::Model *myModel		pointer to the model of the plant to be used in the algorithm
-	@param MPC::Optimizer *qpOASES		pointer to the optimization library to be used in the algorithm
-	@param MPC::Simulator *mySimulator	pointer to the simulator class used to predict the states
-
-	*/
-        void resetMPC( MPC::Model *myModel, MPC::Optimizer *qpOASES, MPC::Simulator *mySimulator);
-
-	/*
-
-	@brief function to initialize the calculation of the MPC algorithm
-	@param
-	@param
-
-	*/
-
-	void initMPCCalc();
-
-	/*
-
-	@brief function to update the MPC algorithm for the next iteration 
-	@param
-	@param
-
-	*/
-
-	void updateMPC();
+            /*
+             @brief function to update the MPC algorithm for the next iteration 
+             @param
+             @param
+             */
+            void updateMPC();
 
 
         protected:
