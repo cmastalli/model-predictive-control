4f8079d6f654d0cf2bb6044bcb555e3979dc3782
carlos.mastalli@gmail.com <carlos.mastalli@gmail.com@d25dd90e-2ac5-73ac-9464-7c43a9925df4>
2013-03-19 22:37:19 +0000

    Improving comments of this code

--- trunk/mpc/include/mpc/Optimizer_Rene.h
+++ trunk/mpc/include/mpc/Optimizer_Rene.h
@@ -4,29 +4,32 @@
 namespace mpc
 {
 
+    /**
+     @class Optimizer
+     @brief Abstract class to define the optimization algorithm of model predictive control
+     */
     class Optimizer		
     {
-	public:
+        public:
         
-        // Constructor
+            /*
+             @brief Constructor function
+             */
+            Optimizer();
 
-        Optimizer();
-
-        // Destructor
-
-        ~Optimizer();
+            /*
+             @brief Destructor function
+             */
+            ~Optimizer();
 	
 
-	/*
-
-	@brief function to define the cost function associated to the MPC problem 
-	@param MPC::Model *myModel		pointer to the process model class "Model"
-	@param int &nWSR			number of working set recalculations
-	@param double *cputime			pointer to the defined time to solve the optimization problem. If NULL, it provides on output 							the actual calculation time of the optimization problem.
-	
-	*/
-        virtual void computeOpt(MPC::Model *myModel, int &nWSR, double *cputime);
-
+            /*
+             @brief function to define the cost function associated to the MPC problem 
+             @param MPC::Model *myModel		pointer to the process model class "Model"
+             @param int &nWSR			number of working set recalculations
+             @param double *cputime			pointer to the defined time to solve the optimization problem. If NULL, it provides on output 							the actual calculation time of the optimization problem.
+             */
+              virtual void computeOpt(MPC::Model *myModel, int &nWSR, double *cputime);
 
 
         protected:
