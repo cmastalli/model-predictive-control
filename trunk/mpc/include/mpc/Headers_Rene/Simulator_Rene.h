173bc5b6e65c100171a8fce4a2d2e16b22b5d661
carlos.mastalli@gmail.com <carlos.mastalli@gmail.com@d25dd90e-2ac5-73ac-9464-7c43a9925df4>
2013-03-19 22:50:35 +0000

    Adding a comments of class. I also change the name of argument of predictState function

--- trunk/mpc/include/mpc/Headers_Rene/Simulator_Rene.h
+++ trunk/mpc/include/mpc/Headers_Rene/Simulator_Rene.h
@@ -4,6 +4,55 @@
 namespace mpc
 {
 
+ 	8
+ 	9
+ 	10
+ 	11
+ 	12
+ 	13
+ 	14
+ 	15
+ 	16
+ 	17
+ 	18
+ 	19
+ 	20
+ 	21
+ 	22
+ 	23
+ 	24
+ 	25
+ 	26
+ 	27
+ 	28
+ 	29
+ 	30
+ 	31
+ 	32
+ 	33
+ 	34
+ 	35
+ 	36
+ 	37
+ 	38
+ 	39
+ 	40
+ 	41
+ 	42
+ 	43
+
+	
+
+#ifndef MPC_OPTIMIZER_H
+#define MPC_OPTIMIZER_H
+
+namespace mpc
+{
+
+    /**
+     @class Simulator
+     @brief Abstract class to compute the dynamic model in order to make a prediction of states
+     */
     class Simulator
     {
 	public:
@@ -18,12 +67,11 @@ namespace mpc
 	     ~Simulator();
 
 	    /**
-	     @brief				function used to obtain the predictions from the process model for a time horizon
-	     @param int *horizonPtr		pointer to the time horizon 
-	     @param MPC::Model *myModel 	pointer to the process model class "Model"
-	    
+	     @brief  Function used to obtain the predictions from the process model for a time horizon
+	     @param int *horizon pointer to the time horizon 
+	     @param mpc::Model *model pointer to the process model class "Model"
 	     */
-	    void predictState(MPC::Model *myModel, int *horizonPtr);
+	    void predictState(mpc::Model *model, int *horizon);
 
 
 	protected:
