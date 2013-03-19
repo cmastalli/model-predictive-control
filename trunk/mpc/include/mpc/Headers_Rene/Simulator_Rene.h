0bf3bc675d97001fc9474051f4ee9e5504e5525e
carlos.mastalli@gmail.com <carlos.mastalli@gmail.com@d25dd90e-2ac5-73ac-9464-7c43a9925df4>
2013-03-19 22:54:36 +0000

    Fixing a writing problem and improving a comments of code

--- trunk/mpc/include/mpc/Headers_Rene/Simulator_Rene.h
+++ trunk/mpc/include/mpc/Headers_Rene/Simulator_Rene.h
@@ -1,50 +1,6 @@
 #ifndef MPC_SIMULATOR_H
 #define MPC_SIMULATOR_H
 
-namespace mpc
-{
-
- 	8
- 	9
- 	10
- 	11
- 	12
- 	13
- 	14
- 	15
- 	16
- 	17
- 	18
- 	19
- 	20
- 	21
- 	22
- 	23
- 	24
- 	25
- 	26
- 	27
- 	28
- 	29
- 	30
- 	31
- 	32
- 	33
- 	34
- 	35
- 	36
- 	37
- 	38
- 	39
- 	40
- 	41
- 	42
- 	43
-
-	
-
-#ifndef MPC_OPTIMIZER_H
-#define MPC_OPTIMIZER_H
 
 namespace mpc
 {
@@ -55,29 +11,33 @@ namespace mpc
      */
     class Simulator
     {
-	public:
-	    /**
-	     @brief Constructor
-	     */
-	     Simulator();
+    
+        public:
+        
+            /*
+             @brief Constructor
+             */
+            Simulator();
 
-	    /**
-	     @brief Destructor
-	     */
-	     ~Simulator();
+            /*
+             @brief Destructor
+             */
+	          ~Simulator();
 
-	    /**
-	     @brief  Function used to obtain the predictions from the process model for a time horizon
-	     @param int *horizon pointer to the time horizon 
-	     @param mpc::Model *model pointer to the process model class "Model"
-	     */
-	    void predictState(mpc::Model *model, int *horizon);
+            /*
+             @brief  Function used to obtain the predictions from the process model for a time horizon
+             @param int *horizon pointer to the time horizon 
+             @param mpc::Model *model pointer to the process model class "Model"
+             */
+            void predictState(mpc::Model *model, int *horizon);
 
 
-	protected:
+       protected:
 	   
-	private:
-	  
+      
+       private:
+  
+  
     };  //@class ModelPredictiveControl
 
 
