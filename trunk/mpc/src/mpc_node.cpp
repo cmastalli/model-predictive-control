49f8f7ceb3dbe240e7c0b8461cb3df6cd60abc31
maidendreamer88@gmail.com <maidendreamer88@gmail.com@d25dd90e-2ac5-73ac-9464-7c43a9925df4>
2013-05-16 17:54:18 +0000

    Modifications in the CMakeLists and Manifest to add the qpOASES library as a dependency.

--- trunk/mpc/src/mpc_node.cpp
+++ trunk/mpc/src/mpc_node.cpp
@@ -25,6 +25,33 @@ Instantiation of the required class objects
 mpc::test_models::Tanksystem();
 mpc::optimizer::Solver_qpOASES();
 
+while (1){
+
+	/*
+	Function callings to perform the neccesary operations
+	*/
+
+	// Reading sensor topic
+	ros::init(argv, argv, "mpc_node");
+	ros::NodeHandle read;
+	
+
+	// Performing the prediction in the horizon
+
+
+	// Precomputing solver parameters
+
+
+	// Compute optimal solution from solver
+
+
+	// Write the current time optimal solution to the actuator system
+
+
+	// Send the rest of optimal inputs to the model
+	
+	
+
 
 
 
