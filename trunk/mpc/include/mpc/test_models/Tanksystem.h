18bc5e99be5cb313e05f1cfbe5d0450d35dce6ef
maidendreamer88@gmail.com <maidendreamer88@gmail.com@d25dd90e-2ac5-73ac-9464-7c43a9925df4>
2013-05-21 20:22:33 +0000

    Header file modification for the example model "Tanksystem"

--- trunk/mpc/include/mpc/test_models/Tanksystem.h
+++ trunk/mpc/include/mpc/test_models/Tanksystem.h
@@ -1,7 +1,7 @@
 #ifndef TANKSYSTEM_H
 #define TANKSYSTEM_H
 
-#include "model.h"
+#include "mpc/model/model.h"
 #include <Eigen/Dense>
 
 namespace mpc
@@ -26,7 +26,7 @@ namespace mpc
 		 	@param current_time 		Time instant
 		 	@param &A 				Reference to the A matrix
 			*/
-			virtual void getModelParameterA(int current_time, MatrixXd& A);
+			virtual void getModelParameterA(Eigen::MatrixXd& A); /*int current_time,*/
 
 			/*
 			@brief Function that provides the model B matrix for the LTI Tank system.
@@ -34,7 +34,7 @@ namespace mpc
 		 	@param current_time 		Time instant
 		 	@param &B 				Reference to the B matrix
 			*/
-			virtual void getModelParameterB(int current_time, MatrixXd& B);
+			virtual void getModelParameterB(Eigen::MatrixXd& B); /*int current_time,*/
 
 			/*
 			@brief Function that provides the model C matrix for the LTI Tank system.
@@ -42,7 +42,7 @@ namespace mpc
 		 	@param current_time 		Time instant
 		 	@param &C 				Reference to the C matrix (already transposed, the function gives the row vector)
 			*/
-			virtual void getModelParameterC(int current_time, MatrixXd& C);
+			virtual void getModelParameterC( Eigen::MatrixXd& C); /*int current_time,*/
 
 		private:
 
@@ -52,25 +52,22 @@ namespace mpc
 			*/		
 
 			// A matrix
-			Matrix2d Ass<< 0.9992, 0.0000
-			-0.000803, 1.001;
+			Eigen::Matrix2d Ass_;
 
 			// B matrix
-			Vector2d Bss<< 0.002551,
-			0.0000;
+			Eigen::Vector2d Bss_;
 
 			// C matrix
 			// The real Css matrix is the transpose of this vector, the function getModelParameterC does this.
-			Vector2d Css<< 0.0000,
-			1.0000;
+			Eigen::Vector2d Css_;
 
-		} // Class Tanksystem
+		}; // Class Tanksystem
 	
 	} // Namespace test_models
 
 } // Namespace mpc
 
-
+#endif
 		
 
 		
